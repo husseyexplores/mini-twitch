244ee124fc991080caaa125fba8d43c27aa8e030
husseyexplores <husseyexplores@gmail.com>
2019-02-17 15:21:02 +0500

    Create header component

---
+++ client/src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+function Header() {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">
+        Mini Twitch
+      </Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+          All Streams
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+Header.propTypes = {}
+
+Header.defaultProps = {}
+
+export default Header
